de014b164e06a74149a84908223f258906fa1473
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-11-09 22:44:16 +0530

    product schema and dummy data

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Grid, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import data from "../../utils/data";
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <SimpleGrid style={["2","3","3","4"]}>
+      <Grid gap={"2rem"}>
+        {data.products.map((product) => (
+          <Grid md={"4"} key={product.name}>
+            <Image w="200px" src={product.image}></Image>
+            <Text>{product.name}</Text>
+          </Grid>
+        ))}
+      </Grid>
+    </SimpleGrid>
+  );
 };
 
 export default ProductPage;
