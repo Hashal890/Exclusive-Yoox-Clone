cbbf3e3ed9be52a55d8022305049ce1e81df7d33
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-11-11 11:20:10 +0530

    login via github added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { sendError } = require("next/dist/server/api-utils");
 const { checkAccount, createAccount, getTokens } = require("../controllers");
+const { getGithubData } = require("../controllers/github.controller");
 const { sendRequiredFieldError } = require("../helper");
 const { userModel } = require("../models");
 
@@ -47,6 +48,21 @@ user.post("/login", async (req, res) => {
     } else {
       return res.status(401).send({ status: false, message: "Credentials mismatch" });
     }
+  } catch (error) {
+    return sendError(res, error);
+  }
+});
+
+user.get("/github", async (req, res) => {
+  const userData = await getGithubData(req.query.code);
+  try {
+    let user = await checkAccount("email", userData.email.toLowerCase());
+    if (!user._id) user = await createAccount(userData.name, userData.email, v4(), true);
+    const { accessToken, refreshToken } = getTokens(user._id, user.name, user.email);
+    return res.send({
+      message: "Login Successfull",
+      data: { accessToken, refreshToken, name: user.name, email: user.email },
+    });
   } catch (error) {
     return sendError(res, error);
   }
