faebff4327e72d0f4e604b700d2061ad9a5ecbcb
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-11-27 18:21:21 +0530

    bug

--- server/helper/sendError.js
+++ server/helper/sendError.js
@@ -1,6 +1,6 @@
 const sendError = (res, err, code = 500) => {
   console.log("SEND Erro", err);
-  return res.status(code).send({ status: false, message: err.message });
+  return res.status(code).send({ status: false, message: err });
 };
 
 module.exports = { sendError };
